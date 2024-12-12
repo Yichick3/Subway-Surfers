81fa68d11737180f6cdce3518707860e83c68e53
Yichick3 <yichick3@gmail.com>
2024-12-11 21:55:54 -0500

    Create player.py

---
+++ player.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Player(pygame.Rect):
+    def __init__(self):
+        super().__init__(100, 100, 50, 50)
+        self.speed = 5
+
+    def move(self, keys):
+        if keys[pygame.K_LEFT]:
+            self.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.x += self.speed
+        if keys[pygame.K_UP]:
+            self.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.y += self.speed
